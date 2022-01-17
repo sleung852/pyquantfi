18b3b6ba13dfa9d8c64f84fcc80396bb6ab1f384
SH Leung <sleung852@gmail.com>
2022-01-17 18:05:51 +0800

    moved cpp gpu to another c++ FMLib

--- cpp/kernel.h
+++
@@ -1,8 +0,0 @@
-#ifndef _KERNEL_CUH_
-#define _KERNEL_CUH_
-
-void run_mc_cuda(double* ST, double S, double sigma, double rate, double T, int n,
-	int m, double* Zs);
-
-#endif
-
--- cpp/montecarlo.h
+++
@@ -1,7 +0,0 @@
-#ifndef _MONTECARLO_H_
-#define _MONTECARLO_H_
-
-void run_mc_cpu(double* ST, const double &S, const double &sigma, const double &rate,
-	const double &T, const int &n, const int &m, double* Zs);
-
-#endif
